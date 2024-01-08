51cef96c24eb00c249041cf5fa7e828f19767de8
aeon <jerry73204@gmail.com>
2024-01-08 17:12:14 +0800

    Add doc comments on Config fields

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,10 @@ use std::time::Duration;
 /// Configuration parameters that are passed to [sync](crate::sync());
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Config {
+    /// The time span that the grouped frames must fit within.
     pub window_size: Duration,
+    /// Accepted minimum timestamps for input frames.
     pub start_time: Option<Duration>,
+    /// The maximum number of frames kept for each input stream.
     pub buf_size: usize,
 }
